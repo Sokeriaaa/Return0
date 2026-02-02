ef961367a0c995e9c0c7bbe4653dadd8237701a6
Sokeriaaa <73162265+Sokeriaaa@users.noreply.github.com>
2026-02-02 17:20:23 +0800

    Add onIntent test for ShopViewModel.kt

--- composeApp/src/commonTest/kotlin/sokeriaaa/return0/test/mvi/viewmodels/ShopViewModelTest.kt
+++ composeApp/src/commonTest/kotlin/sokeriaaa/return0/test/mvi/viewmodels/ShopViewModelTest.kt
@@ -19,23 +19,25 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
-import sokeriaaa.return0.models.component.context.EventContext
 import sokeriaaa.return0.mvi.intents.ShopIntent
 import sokeriaaa.return0.mvi.viewmodels.ShopViewModel
 import sokeriaaa.return0.shared.data.api.story.event.interactive.buildShop
 import sokeriaaa.return0.shared.data.models.story.currency.CurrencyType
-import sokeriaaa.return0.test.applib.modules.TestKoinModules
+import sokeriaaa.return0.test.annotations.AppRunner
+import sokeriaaa.return0.test.annotations.RunWith
 import sokeriaaa.return0.test.models.story.event.BaseEventTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
 @OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(AppRunner::class)
 class ShopViewModelTest : BaseEventTest() {
 
     @Test
     fun `Initialize executes correctly`() {
-        withViewModel {
-            withContext(callback = object : TestingCallback() {}) {
+        withContext(callback = object : TestingCallback() {}) {
+            withViewModel {
                 onIntent(
                     ShopIntent.Initialize(
                         context = it,
@@ -45,27 +47,236 @@ class ShopViewModelTest : BaseEventTest() {
                         },
                     ),
                 )
+                // Keys
+                assertEquals(
+                    expected = listOf("awesome_item", "epic_item"),
+                    actual = items.keys.toList(),
+                )
+                // Prices
+                assertEquals(
+                    expected = listOf(100 to CurrencyType.TOKEN, 5 to CurrencyType.CRYPTO),
+                    actual = items.values.map { it.price },
+                )
             }
-            // Keys
-            assertEquals(
-                expected = listOf("awesome_item", "epic_item"),
-                actual = items.keys.toList(),
-            )
-            // Prices
-            assertEquals(
-                expected = listOf(100 to CurrencyType.TOKEN, 5 to CurrencyType.CRYPTO),
-                actual = items.values.map { it.price },
-            )
+        }
+    }
+
+    @Test
+    fun `AlterCart executes correctly - Normal`() {
+        withContext(callback = object : TestingCallback() {}) {
+            withViewModel {
+                // Initialize shop
+                onIntent(
+                    ShopIntent.Initialize(
+                        context = it,
+                        shopEvent = buildShop("testing_shop") {
+                            inventory("awesome_item") soldFor 100.token
+                            inventory("epic_item") soldFor 5.crypto
+                        },
+                    ),
+                )
+                // Add to cart
+                onIntent(ShopIntent.AlterCart("awesome_item", 42))
+                assertEquals(
+                    expected = mapOf("awesome_item" to 42),
+                    actual = cartItems.associate { it.first.key to it.second },
+                )
+                // Decrease amount
+                onIntent(ShopIntent.AlterCart("awesome_item", -24))
+                assertEquals(
+                    expected = mapOf("awesome_item" to 18),
+                    actual = cartItems.associate { it.first.key to it.second },
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `AlterCart executes correctly - Upper limit`() {
+        withContext(callback = object : TestingCallback() {}) {
+            withViewModel {
+                // Initialize shop
+                onIntent(
+                    ShopIntent.Initialize(
+                        context = it,
+                        shopEvent = buildShop("testing_shop") {
+                            inventory("awesome_item") soldFor 100.token limitFor 10
+                            inventory("epic_item") soldFor 5.crypto
+                        },
+                    ),
+                )
+                // Add to cart
+                onIntent(ShopIntent.AlterCart("awesome_item", 42))
+                assertEquals(
+                    expected = mapOf("awesome_item" to 10),
+                    actual = cartItems.associate { it.first.key to it.second },
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `RemoveFromCart executes correctly`() {
+        withContext(callback = object : TestingCallback() {}) {
+            withViewModel {
+                // Initialize shop
+                onIntent(
+                    ShopIntent.Initialize(
+                        context = it,
+                        shopEvent = buildShop("testing_shop") {
+                            inventory("awesome_item") soldFor 100.token
+                            inventory("epic_item") soldFor 5.crypto
+                        },
+                    ),
+                )
+                // Add to cart
+                onIntent(ShopIntent.AlterCart("awesome_item", 42))
+                assertEquals(
+                    expected = mapOf("awesome_item" to 42),
+                    actual = cartItems.associate { it.first.key to it.second },
+                )
+                // Remove from cart
+                onIntent(ShopIntent.RemoveFromCart("awesome_item"))
+                assertFalse { cartItems.any { it.first.key == "awesome_item" } }
+            }
+        }
+    }
+
+    @Test
+    fun `Buy executes correctly - Normal`() {
+        withContext(callback = object : TestingCallback() {}) {
+            withViewModel {
+                // Initialize currency
+                it.gameState.currency[CurrencyType.TOKEN] += 5000
+                // Initialize shop
+                onIntent(
+                    ShopIntent.Initialize(
+                        context = it,
+                        shopEvent = buildShop("testing_shop") {
+                            inventory("awesome_item") soldFor 100.token
+                            inventory("epic_item") soldFor 5.crypto
+                        },
+                    ),
+                )
+                // Buy
+                onIntent(
+                    intent = ShopIntent.Buy(
+                        key = "awesome_item",
+                        amount = 1,
+                        onPurchased = {
+                            // Asserts
+                            assertEquals(4900, it.gameState.currency[CurrencyType.TOKEN])
+                            assertEquals(1, it.gameState.inventory["awesome_item"])
+                        },
+                    )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `Buy executes correctly - Insufficient balance`() {
+        withContext(callback = object : TestingCallback() {}) {
+            withViewModel {
+                // Initialize currency
+                it.gameState.currency[CurrencyType.TOKEN] += 5000
+                // Initialize shop
+                onIntent(
+                    ShopIntent.Initialize(
+                        context = it,
+                        shopEvent = buildShop("testing_shop") {
+                            inventory("awesome_item") soldFor 100.token
+                            inventory("epic_item") soldFor 5.crypto
+                        },
+                    ),
+                )
+                // Buy
+                onIntent(
+                    intent = ShopIntent.Buy(
+                        key = "awesome_item",
+                        amount = 100,
+                        onPurchased = {
+                            // Purchase failed
+                            assertEquals(5000, it.gameState.currency[CurrencyType.TOKEN])
+                            assertEquals(0, it.gameState.inventory["awesome_item"])
+                        },
+                    ),
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `Checkout executes correctly - Normal`() {
+        withContext(callback = object : TestingCallback() {}) {
+            withViewModel {
+                // Initialize currency
+                it.gameState.currency[CurrencyType.TOKEN] += 5000
+                it.gameState.currency[CurrencyType.CRYPTO] += 10
+                // Initialize shop
+                onIntent(
+                    ShopIntent.Initialize(
+                        context = it,
+                        shopEvent = buildShop("testing_shop") {
+                            inventory("awesome_item") soldFor 100.token
+                            inventory("epic_item") soldFor 5.crypto
+                        },
+                    ),
+                )
+                // Add to cart
+                onIntent(ShopIntent.AlterCart("awesome_item", 3))
+                onIntent(ShopIntent.AlterCart("epic_item", 1))
+                onIntent(
+                    intent = ShopIntent.CheckOut(
+                        onPurchased = {
+                            // Asserts
+                            assertEquals(4700, it.gameState.currency[CurrencyType.TOKEN])
+                            assertEquals(5, it.gameState.currency[CurrencyType.CRYPTO])
+                            assertEquals(3, it.gameState.inventory["awesome_item"])
+                            assertEquals(1, it.gameState.inventory["epic_item"])
+                        }
+                    ),
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `Checkout executes correctly - Insufficient balance`() {
+        withContext(callback = object : TestingCallback() {}) {
+            withViewModel {
+                // Initialize currency
+                it.gameState.currency[CurrencyType.TOKEN] += 5000
+                it.gameState.currency[CurrencyType.CRYPTO] += 10
+                // Initialize shop
+                onIntent(
+                    ShopIntent.Initialize(
+                        context = it,
+                        shopEvent = buildShop("testing_shop") {
+                            inventory("awesome_item") soldFor 100.token
+                            inventory("epic_item") soldFor 5.crypto
+                        },
+                    ),
+                )
+                // Add to cart
+                onIntent(ShopIntent.AlterCart("awesome_item", 3))
+                onIntent(ShopIntent.AlterCart("epic_item", 10))
+                onIntent(ShopIntent.CheckOut {})
+                // Purchase failed
+                assertEquals(5000, it.gameState.currency[CurrencyType.TOKEN])
+                assertEquals(10, it.gameState.currency[CurrencyType.CRYPTO])
+                assertEquals(0, it.gameState.inventory["awesome_item"])
+                assertEquals(0, it.gameState.inventory["epic_item"])
+            }
         }
     }
 
     private inline fun withViewModel(
-        block: ShopViewModel.(EventContext) -> Unit,
+        block: ShopViewModel.() -> Unit,
     ) {
-        TestKoinModules.withModules {
-            Dispatchers.setMain(StandardTestDispatcher())
-            val viewModel = ShopViewModel()
-            Dispatchers.resetMain()
-        }
+        Dispatchers.setMain(StandardTestDispatcher())
+        val viewModel = ShopViewModel()
+        viewModel.block()
+        Dispatchers.resetMain()
     }
 }
