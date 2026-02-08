96cad214b2bcc86f98c3403e9bcf558d423a411c
Sokeriaaa <73162265+Sokeriaaa@users.noreply.github.com>
2026-02-08 15:54:37 +0800

    Half-finished AppLanguageHandler.

---
+++ composeApp/src/commonMain/kotlin/sokeriaaa/return0/ui/common/locale/AppLanguageHandler.kt
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2026 Sokeriaaa
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+package sokeriaaa.return0.ui.common.locale
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.intl.Locale
+
+/**
+ * Language handler.
+ *
+ * @param languageCode Language code. Format is "en-US".
+ */
+@Composable
+fun AppLanguageHandler(
+    languageCode: String,
+    content: @Composable () -> Unit
+) {
+    val locale = remember(languageCode) {
+        Locale(languageCode)
+    }
+    content()
+}
